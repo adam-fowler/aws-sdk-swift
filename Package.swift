d963b89419e88864cf88f5a9e698b23c38c55271
Adam Fowler <adamfowler71@gmail.com>
2020-06-11 20:37:54 +0100

    Use debug branch as aws-sdk-swift-core

--- Package.swift
+++ Package.swift
@@ -245,7 +245,7 @@ let package = Package(
         .library(name: "AWSXRay", targets: ["AWSXRay"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .branch("master"))
+        .package(url: "https://github.com/adam-fowler/aws-sdk-swift-core.git", .branch("debug"))
     ],
     targets: [
         .target(name: "AWSACM", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/AWSSDKSwift/Services/ACM"),
